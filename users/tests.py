b879942dac67eb1042402d321acfe870bd0bcb14
Ali <ak.msm91@gmail.com>
2020-04-16 04:34:54 -0500

    ch3

---
+++ users/tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='Ali2',
+            email='test@gmail.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'Ali2')
+        self.assertEqual(user.email, 'test@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='super@gmail.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'super@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
