7411b84e6e3de0c739234c21b67c0237f9714f38
Ali <ak.msm91@gmail.com>
2020-04-24 08:11:17 -0500

    ch14

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     'books.apps.BooksConfig',
+    'orders.apps.OrdersConfig',
 
     # 3rd party
     'crispy_forms',
@@ -167,3 +168,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STRIPE_TEST_PUBLISHABLE_KEY=os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
+STRIPE_TEST_SECRET_KEY=os.environ.get('STRIPE_TEST_SECRET_KEY')
